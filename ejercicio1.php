67ace2bd2ad7cdb94ca800aef63dd541e6213c05
GiulianoPiazza <giulipiazza5@gmail.com>
2022-10-27 20:01:03 -0300

    cambie el archivo ejercicio1.php

--- ejercicio1.php
+++ ejercicio1.php
@@ -2,7 +2,7 @@
 /* Hacer los algoritmos que calculen el promedio de una serie de notas hasta que se ingrese una nota 
 negativa, utilizar las tres estructuras repetitivas. ¿Cuál no se puede utilizar? */
 
-echo "--------CALCULADOR DE PROMEDIOS--------" . PHP_EOL;
+echo "--------CALCULADOR DE PROMEDIOS--------". PHP_EOL;
 echo " Para finalizarlo, ingresa un numero negativo." . PHP_EOL;
 
 
